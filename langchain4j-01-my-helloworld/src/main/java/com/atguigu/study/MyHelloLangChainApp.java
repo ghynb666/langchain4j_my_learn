b0c570b4449ff6592fa65a37c991f5eed9496665
郭浩宇 <2290399336@qq.com>
2026-02-18 17:47:07 +0800

    feat(app): 新增 Spring Boot 启动类与依赖配置
    - 添加 MyHelloLangChainApp 启动类，实现应用入口
    - 配置 Spring Boot 相关依赖，支持web启动及测试
    - 集成 langchain4j-open-ai 和 langchain4j，支持 LLM 调用
    - 引入 Lombok 方便开发，添加 maven 插件优化打包流程
    - 更新 pom.xml 文件结构，完善项目信息和属性配置

---
+++ langchain4j-01-my-helloworld/src/main/java/com/atguigu/study/MyHelloLangChainApp.java
@@ -0,0 +1,20 @@
+package com.atguigu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:MyHelloLangChainApp
+ * Package:com.atguigu.study
+ * Description:
+ *
+ * @Author 郭浩宇
+ * @Create 2026/2/18 17:46
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MyHelloLangChainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyHelloLangChainApp.class, args);
+    }
+}
